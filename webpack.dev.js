b22a5dbbe5b0fbcdbb45d0713852568d194f7f66
McLisak <McLisak@gmail.com>
2019-03-06 21:51:07 +0100

    Further dev setup

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,27 +1,74 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 const webpack = require('webpack');
+const fs = require('fs');
+
+const getEntries = () => {
+  const result = {
+    index: path.resolve(__dirname, 'src/index.js'),
+  };
+  const exclude = ['assets'];
+
+  const extensionRegex = new RegExp(/^.*\.[^\\]+$/);
+  const srcFiles = fs.readdirSync('src');
+  srcFiles.forEach((fileName) => {
+    if (exclude.includes(fileName) || extensionRegex.test(fileName)) {
+      return;
+    }
+    let entryName = `${fileName}/index`;
+    result[entryName] = path.resolve(__dirname, 'src/' + entryName);
+  });
+  return result;
+};
+
+const getHtmlPlugins = () => {
+  const result = [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      chunks: ['index'],
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+  ];
+  const exclude = ['assets'];
+  const extensionRegex = new RegExp(/^.*\.[^\\]+$/);
+  const srcFiles = fs.readdirSync('src');
+  srcFiles.forEach((fileName) => {
+    if (exclude.includes(fileName) || extensionRegex.test(fileName)) {
+      return;
+    }
+    let entryName = `${fileName}/index.html`;
+    result.push(
+      new HtmlWebpackPlugin({
+        filename: entryName,
+        template: path.resolve(__dirname, 'src/' + entryName),
+        chunks: [fileName + '/index'],
+        base: '/' + fileName,
+      })
+    );
+  });
+  return result;
+};
 
 module.exports = () => {
   const config = {
     mode: 'development',
     devServer: {
-      contentBase: 'dist',
+      contentBase: path.resolve(__dirname, 'dist'),
       host: '0.0.0.0',
-      hot: true,
     },
-    entry: {
-      'test1/index': './src/test1/index.js',
-      index: './src/index.js',
-    },
+    entry: getEntries(),
     output: {
+      path: path.resolve(__dirname, 'dist'),
       filename: '[name].js',
     },
     module: {
       rules: [
         {
           test: /\.scss$/,
-          use: ['css-loader', 'postcss-loader', 'sass-loader'],
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
         },
       ],
     },
@@ -32,28 +79,12 @@ module.exports = () => {
     },
     plugins: [
       new CleanWebpackPlugin(),
-      new CopyWebpackPlugin(
-        [
-          {
-            context: 'src/',
-            from: '**/*.html',
-          },
-        ],
-        { logLevel: 'debug' }
-      ),
+      ...getHtmlPlugins(),
+      new MiniCssExtractPlugin({
+        chunkFilename: '[name].css',
+      }),
       new webpack.HotModuleReplacementPlugin(),
     ],
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            test: /node_modules/,
-            name: 'vendors',
-            chunks: 'initial',
-          },
-        },
-      },
-    },
   };
 
   return config;
